085365dbe0485d70618551803bfbaee6d1abcf9f
Alexey Naiden <17383+anayden@users.noreply.github.com>
2023-12-13 00:06:25 +0400

    Update workflows

--- noxfile.py
+++ noxfile.py
@@ -27,11 +27,11 @@ python_versions = ["3.9"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
-    "safety",
+    # "safety",
     "mypy",
     "tests",
-    "typeguard",
-    "xdoctest",
+    # "typeguard",
+    # "xdoctest",
     "docs-build",
 )
 
